be2ef9a1d14acfab1a9e11c0ed79c832f9733028
Karl Kablisk <karlkablisk@gmail.com>
2023-07-28 16:03:33 +0900

    Update player.py

--- player.py
+++ player.py
@@ -1,1 +1,30 @@
-#all player related details go here
+#player.py - all player related details go here
+
+import streamlit as st
+from my_pixabay import fetch_image
+
+PIXABAY_API_KEY = st.secrets["PIXABAY_API_SECRET"]
+
+def generate_player():
+    player = {}
+    player_image = fetch_image('person', PIXABAY_API_KEY)
+    player["image"] = player_image
+    player["name"] = "PLAYER"
+    player["class"] = "CLASS_NAME" # Placeholder
+    player["stats"] = {
+        "Strength": 0,
+        "Charisma": 0,
+        "Dexterity": 0,
+        "Agility": 0,
+        "Luck": 0
+    }
+    player["items"] = []
+    player["equipment"] = {
+        "head": None,
+        "body": None,
+        "arms": None,
+        "legs": None,
+        "accessory1": None,
+        "accessory2": None
+    }
+    return player
