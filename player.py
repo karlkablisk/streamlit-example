c9a5e50f18fbb928fa2047fb0089400b7d6db477
Karl Kablisk <karlkablisk@gmail.com>
2023-07-28 16:16:01 +0900

    Update player.py

--- player.py
+++ player.py
@@ -3,27 +3,53 @@
 import streamlit as st
 from my_pixabay import fetch_image
 
+# Player attributes
+player_image = fetch_image("person")
+player_name = "PLAYER"
+player_class = "Warrior"  # Example class for now
 
-def generate_player():
-    player = {}
-    player_image = fetch_image('person', PIXABAY_API_KEY)
-    player["image"] = player_image
-    player["name"] = "PLAYER"
-    player["class"] = "CLASS_NAME" # Placeholder
-    player["stats"] = {
-        "Strength": 0,
-        "Charisma": 0,
-        "Dexterity": 0,
-        "Agility": 0,
-        "Luck": 0
-    }
-    player["items"] = []
-    player["equipment"] = {
-        "head": None,
-        "body": None,
-        "arms": None,
-        "legs": None,
-        "accessory1": None,
-        "accessory2": None
-    }
-    return player
+player_stats = {
+    "Strength": 10,
+    "Charisma": 10,
+    "Dexterity": 10,
+    "Agility": 10,
+    "Luck": 10
+}
+
+player_items = ["Item1", "Item2"]  # Example items for now
+
+player_equipment = {
+    "Head": "None",
+    "Body": "None",
+    "Arms": "None",
+    "Legs": "None",
+    "Accessory 1": "None",
+    "Accessory 2": "None"
+}
+
+# Functions to retrieve player attributes
+def get_player_image():
+    return player_image
+
+def get_player_name():
+    return player_name
+
+def set_player_name(name):
+    global player_name
+    player_name = name
+
+def get_player_class():
+    return player_class
+
+def get_stat(stat_name):
+    return player_stats.get(stat_name, "N/A")
+
+def get_items():
+    return player_items
+
+def get_equipment(slot):
+    return player_equipment.get(slot, "None")
+
+def set_equipment(slot, item):
+    if slot in player_equipment:
+        player_equipment[slot] = item
