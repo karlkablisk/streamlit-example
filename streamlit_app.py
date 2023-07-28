21f206c85f185725e7e8fabccd1f609905f94548
Karl Kablisk <karlkablisk@gmail.com>
2023-07-28 16:18:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ import interface
 import player
 import chat
 from my_pixabay import fetch_image, fetch_video
-from player import generate_player
 from interface import sidebar_area, column1, column2, column3
 
 def main():
