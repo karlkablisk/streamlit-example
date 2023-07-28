1245187f22fd1e3c09506b2cf4dbe8e5bee36d62
Karl Kablisk <karlkablisk@gmail.com>
2023-07-28 16:31:23 +0900

    Update player.py

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 #player.py - all player related details go here
 
+
 import streamlit as st
 from my_pixabay import fetch_image
 
@@ -27,33 +28,17 @@ player_equipment = {
     "Accessory 2": "None"
 }
 
-def generate_player():
-    # Sample logic to generate player
-    player_data = {
-        "name": "Default Player",
-        "class": "Warrior",  # Default class
-        "stats": {
-            "Strength": 10,
-            "Charisma": 10,
-            "Dexterity": 10,
-            "Agility": 10,
-            "Luck": 10
-        },
-        "items": [],
-        "equipment": {
-            "head": None,
-            "body": None,
-            "arms": None,
-            "legs": None,
-            "accessory1": None,
-            "accessory2": None
-        }
-    }
-    return player_data
+# Generate player method removed as we're directly using global variables
 
 # Functions to retrieve player attributes
 def get_player_image():
     return player_image
+
+def reroll_player_image(keyword):
+    global player_image
+    new_image_url = fetch_image(keyword)
+    if new_image_url:
+        player_image = new_image_url
 
 def get_player_name():
     return player_name
@@ -75,5 +60,6 @@ def get_equipment(slot):
     return player_equipment.get(slot, "None")
 
 def set_equipment(slot, item):
+    global player_equipment
     if slot in player_equipment:
         player_equipment[slot] = item
