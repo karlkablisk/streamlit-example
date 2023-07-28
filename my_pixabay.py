925de412383dc250a466e38e84e1baa8419ad03d
Karl Kablisk <karlkablisk@gmail.com>
2023-07-28 16:02:38 +0900

    Create my_pixabay.py

---
+++ my_pixabay.py
@@ -0,0 +1,43 @@
+#my_pixabay.py - all pixabay logic goes here
+
+import streamlit as st
+import random
+import requests
+
+BASE_IMAGE_URL = "https://pixabay.com/api/"
+BASE_VIDEO_URL = "https://pixabay.com/api/videos/"
+
+def fetch_image(query, api_key):
+    params = {
+        'key': api_key,
+        'q': query,
+        'image_type': 'photo',
+        'pretty': 'true'
+    }
+
+    response = requests.get(BASE_IMAGE_URL, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data['totalHits'] > 0:
+            return random.choice(data['hits'])['webformatURL']
+        else:
+            return None
+    else:
+        return None
+
+def fetch_video(query, api_key):
+    params = {
+        'key': api_key,
+        'q': query,
+        'pretty': 'true'
+    }
+
+    response = requests.get(BASE_VIDEO_URL, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data['totalHits'] > 0:
+            return random.choice(data['hits'])['videos']['medium']['url']
+        else:
+            return None
+    else:
+        return None
